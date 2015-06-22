c0fc6598bcb013ea078bc2a7a6ee5e86d5963b46
Vojtěch Smejkal <smejkalv@gmail.com>
2015-06-23 00:28:56 +0200

    models: Event, Place

---
+++ src/model/event.go
@@ -0,0 +1,15 @@
+package model/event
+
+import "time"
+
+type Event struct {
+	Title string
+	Desc string
+	Link string
+	Image string
+	Categories []string
+	Start time.Time
+	End time.Time
+	IsDateOnly bool
+	Place Place
+}
---
+++ src/model/place.go
@@ -0,0 +1,11 @@
+package model/place
+
+type Place struct {
+	Title string
+	Categories []string
+    Latitude float
+    Longitude float
+	City string
+    Street string
+    Zip int
+}
